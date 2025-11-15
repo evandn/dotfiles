4367f1dcbff930a45f03252eb59c7a7789117941
Evan Duong <evduong@icloud.com>
2025-11-15 15:33:22 +0700

    Use full-upgrade instead of upgrade for Linux

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -Eeuo pipefail
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
 
 # Install essential packages for Linux
-[[ "$OS" == 'linux' ]] && sudo apt update && sudo apt upgrade -y && sudo apt install -y build-essential git
+[[ "$OS" == 'linux' ]] && sudo apt update && sudo apt full-upgrade -y && sudo apt install -y build-essential git
 
 # Clone dotfiles repo if missing
 test -d "$HOME/dotfiles" || GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no' git clone git@github.com:evandn/dotfiles.git "$_" && cd "$_"
